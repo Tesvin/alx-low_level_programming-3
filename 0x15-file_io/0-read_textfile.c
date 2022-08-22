28904cd000b5953eddea571c1d9dc9d9d9eae82f
stephen <stephenazosike@gmail.com>
2022-08-22 15:57:25 -0700

    commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -34,6 +34,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 															return (0);
 
 														close(file);
-														free(buf);
-														return (wr);
+	free(buf);
+	return (wr);
 }
